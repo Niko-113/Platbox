e39938fa04af6f4da58da583ad570d5a27538e44
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-09-26 18:32:34 -0700

    2nd Player

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -8,6 +8,10 @@ var speed = 15;
 var max_speed = 250;
 var jump_strength = 300;
 
+export var move_right = "move_right";
+export var move_left = "move_left";
+export var move_up = "move_up";
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,19 +26,19 @@ func _integrate_forces(state):
 	
 	var grounded = state.get_contact_count() > 0;
 	
-	var move_right = Input.is_action_pressed("ui_right");
-	var move_left = Input.is_action_pressed("ui_left");
-	var jump = Input.is_action_just_pressed("ui_up");
+	var x_right = Input.is_action_pressed(move_right);
+	var x_left = Input.is_action_pressed(move_left);
+	var jump = Input.is_action_just_pressed(move_up);
 	
 	if jump and grounded:
 		state.apply_central_impulse(Vector2.UP * jump_strength);
 		
 	var vec = state.get_linear_velocity();
 	
-	vec.x += int(move_right) * speed;
+	vec.x += int(x_right) * speed;
 	vec.x = min(vec.x, max_speed);
 	
-	vec.x -= int(move_left) * speed;
+	vec.x -= int(x_left) * speed;
 	vec.x = max(vec.x, -max_speed);
 	
 	state.set_linear_velocity(vec);
