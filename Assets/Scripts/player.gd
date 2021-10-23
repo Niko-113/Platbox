7c636b90ce563403a5f40e7018b9304de99196b6
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-10-22 18:29:28 -0700

    Switched to KinematicBody
    Players now use Kinematic Bodies instead of Rigidbody. Also merged both player prefabs into one.

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -1,9 +1,12 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 
-var speed = 15;
-var max_speed = 250;
-var jump_strength = 300;
+var speed = 250
+var jump_strength = -500
+var gravity = 1000
+
+var jumping = false
+var velocity = Vector2()
 
 export var move_right = "move_right"
 export var move_left = "move_left"
@@ -29,34 +32,61 @@ func _process(delta):
 		_sprite.flip_v = true
 	else:
 		_sprite.flip_v = false
-func _integrate_forces(state):
+		
+
+# DEFUNCT RIGIDBODY CODE - DELETE LATER
+#func _integrate_forces(state):
+#	var _opponent = get_node(opponent_path)
+#	self.look_at(Vector2(_opponent.position.x, self.position.y))
+#
+#	if Input.is_action_just_pressed(attack):
+#		_animator.play("PlayerAttackAnim")
+#
+#	var grounded = state.get_contact_count() > 0;
+#
+#	var x_right = Input.is_action_pressed(move_right);
+#	var x_left = Input.is_action_pressed(move_left);
+#	var jump = Input.is_action_just_pressed(move_up);
+#
+#	if jump and grounded:
+#		state.apply_central_impulse(Vector2.UP * jump_strength);
+#
+#	var vec = state.get_linear_velocity();
+#
+#	vec.x += int(x_right) * speed;
+#	vec.x = min(vec.x, max_speed);
+#
+#	vec.x -= int(x_left) * speed;
+#	vec.x = max(vec.x, -max_speed);
+#
+#	state.set_linear_velocity(vec);
+
+func _physics_process(delta):
 	var _opponent = get_node(opponent_path)
 	self.look_at(Vector2(_opponent.position.x, self.position.y))
-	
 	if Input.is_action_just_pressed(attack):
 		_animator.play("PlayerAttackAnim")
-	
-	var grounded = state.get_contact_count() > 0;
 	
 	var x_right = Input.is_action_pressed(move_right);
 	var x_left = Input.is_action_pressed(move_left);
 	var jump = Input.is_action_just_pressed(move_up);
 	
-	if jump and grounded:
-		state.apply_central_impulse(Vector2.UP * jump_strength);
+	velocity.x = (int(x_right) - int(x_left)) * speed
+	if is_on_floor():
+		jumping = false
+		velocity.y = 0
+	if jump and is_on_floor():
+		#jumping = true
+		velocity.y = jump_strength
 		
-	var vec = state.get_linear_velocity();
 	
-	vec.x += int(x_right) * speed;
-	vec.x = min(vec.x, max_speed);
+	velocity.y += gravity * delta
 	
-	vec.x -= int(x_left) * speed;
-	vec.x = max(vec.x, -max_speed);
-	
-	state.set_linear_velocity(vec);
+	move_and_slide(velocity, Vector2(0, -1))
 
 func respawn():
 	var respawn_point = get_node(respawn_path)
 	self.position = respawn_point.position
-	self.linear_velocity = Vector2()
+	#self.linear_velocity = Vector2()
+	velocity = Vector2()
 	_animator.advance(1)
